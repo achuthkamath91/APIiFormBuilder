0e42c0e38cbbb12053f718531e0e8b9efef471e8
Achuthkamath91 <achuth.kamath91@gmail.com>
2017-06-15 17:03:27 -0400

    added env data in config

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
-//define("acessToken", "2bddbea29b731031ec05fd83066b59f373a3baf2");
-define("CLIENT_SECRET", "ce68eca2a84d8aa565e31714555fa45892adedad");
-define("CLIENT_KEY", "5b35b692d47cf67f7d717ce08e91feef40e46d26");
+$CS = getenv('CLIENT_SECRET');
+$CK = getenv('CLIENT_KEY');
+$PageId = getenv('PAGE_ID');
+$ProfileId = getenv('PROFILE_ID');
+define("CLIENT_SECRET", $CS);
+define("CLIENT_KEY", $CK);
 define("AUD_VALUE", "https://app.iformbuilder.com/exzact/api/oauth/token");
-define("PAGE_ID",3636918);
-define("PROFILE_ID",469902);
-define("RECORDURL","https://app.iformbuilder.com/exzact/api/v60/profiles/469902/pages/3636918/records");
+define("PAGE_ID", $PageId);
+define("PROFILE_ID",$ProfileId);
+define("RECORDURL","https://app.iformbuilder.com/exzact/api/v60/profiles/".PROFILE_ID."/pages/".PAGE_ID."/records");
 ?>
